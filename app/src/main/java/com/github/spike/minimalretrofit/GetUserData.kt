2b33f6453ac43d169e130e8e3fd15eedaef48938
Stephan <stephan@branczyk.org>
2022-11-03 16:32:40 -0700

    added a comment that reminds me to add an attribute in case the http connection is not secure

--- app/src/main/java/com/github/spike/minimalretrofit/GetUserData.kt
+++ app/src/main/java/com/github/spike/minimalretrofit/GetUserData.kt
@@ -6,6 +6,8 @@ import retrofit2.http.GET
 import retrofit2.converter.gson.GsonConverterFactory
 
 val baseUrl = "https://api.github.com/"
+// if http instead of http, be sure to add
+// android:usesCleartextTraffic="true" to application inside manifest
 
 interface GetUserData {
     @GET("users")
